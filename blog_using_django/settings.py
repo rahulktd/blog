31248a17c77e03a25a14920da0f29eac3f0bae16
rahulktd <rahulkpktd@gmail.com>
2023-07-07 15:35:33 +0530

    Can update posts. See posts.

--- blog_using_django/settings.py
+++ blog_using_django/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
 'OPTIONS': {
-            'version': '3.8.11.1'  # Replace with the desired SQLite version
+            'deterministic': False
         },
     }
 }
